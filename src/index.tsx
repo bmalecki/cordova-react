8ac4f13bdbc53023d396e408acf960e6d09a4a09
bmalecki <bmalecki995@gmail.com>
2016-08-28 16:59:26 +0200

    add tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import "babel-polyfill";
 
-import * as React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
+import * as React from "react";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
 
-import thunk from 'redux-thunk';
-import {Router, Route, hashHistory as _history} from 'react-router';
-import {syncHistoryWithStore, routerMiddleware} from 'react-router-redux';
+import thunk from "redux-thunk";
+import {Route, Router, hashHistory as _history} from "react-router";
+import {syncHistoryWithStore, routerMiddleware} from "react-router-redux";
 
-import configureStore from './store/configureStore';
-import {App} from './components/App';
+import configureStore from "./store/configureStore";
+import {App} from "./components/App";
 
 require('./style/app.scss');
 
-//apply middleware in array
+// apply middleware in array
 const store = configureStore([
-  routerMiddleware(_history), thunk  
+  routerMiddleware(_history), thunk
 ]);
 const history = syncHistoryWithStore(_history, store);
 
@@ -26,5 +26,5 @@ render(
       </Route>
     </Router>
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 )
--- src/tests/some.spec.tsx
+++ src/tests/some.spec.tsx
@@ -1,6 +1,6 @@
-import {chai, enzyme, sinon} from './helpers';
+import {chai, enzyme, sinon} from "./helpers";
 import * as React from "react";
-import {Footer} from '../components/Footer';
+import {Footer} from "../components/Footer";
 
 const {assert, expect} = chai;
 const {mount, shallow} = enzyme;
@@ -9,42 +9,40 @@ const {mount, shallow} = enzyme;
 const Test = () => <div className="test"/>;
 
 class Test2 extends React.Component<{}, {}> {
-    render() {
+    public render() {
         return <div/>;
     }
 }
 
 
-describe('Footer', () => {
-  it('should be a function', () => {
+describe("Footer", () => {
+  it("should be a function", () => {
     assert.isFunction(Footer);
   });
 });
 
-describe('test react with chai', () => {
-  it('run exmaple code', () => {
+describe("test react with chai", () => {
+  it("run exmaple code", () => {
     const wrapper = shallow(<Test />);
     expect(wrapper).to.be.not.null;
     expect(wrapper).to.be.checked;
     expect(wrapper).to.have.className("test");
     expect(wrapper).to.not.have.exactly(1).descendants(Test2);
-  });  
+  });
 });
 
-describe('test chai sinon',() => {
-  it('run example', () => {
-    
-    let user = {    
-      setName: function(name){
+describe("test chai sinon",() => {
+  it("run example", () => {
+    let user = {
+      setName: (name) => {
         this.name = name;
       }
     }
 
-    let setNameSpy = sinon.spy(user, 'setName');
+    let setNameSpy = sinon.spy(user, "setName");
 
-    user.setName('Darth Vader');
+    user.setName("Darth Vader");
     setNameSpy.restore();
     expect(setNameSpy).to.have.been.calledWith("Darth Vader");
-    
   });
-});
+})
